0889aa2a41f6f928e63bfadcd2a22c5713754882
Muhammad Fatih <muhammadfatih4994@gmail.com>
2021-11-04 10:11:07 +0700

    trending products

--- route/get-trending-products.js
+++ route/get-trending-products.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const product = require('../models/product')
 
 async function trendingProducts(req,res){
-	const result = await product.find().sort({amount_sold:-1}).limit(req.params)
+	product.find().sort({amount_sold:-1}).limit(req.params).exec((err,product) => {return res.send(product)})
 }
 
 module.exports = trendingProducts
