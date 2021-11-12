b4e663396749970bbd25487844e8f16b2922b167
Muhammad Fatih <muhammadfatih4994@gmail.com>
2021-11-12 10:34:46 +0700

    fix new arrival

--- route/get-new-arrival-products.js
+++ route/get-new-arrival-products.js
@@ -3,7 +3,7 @@ const product = require('../models/product')
 
 async function newProducts(req,res){
 	const {amount = 100} = req.params
-	product.find().sort({created_at:-1}).limit(parseInt(amount)).exec((err,product) => {return res.send(product)})
+	product.find().sort({created_at:-1}).limit(parseInt(amount)).exec((product) => {return res.send(product)})
 }
 
 module.exports = newProducts
